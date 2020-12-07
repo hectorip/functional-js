63ce365065b2023d634e24b898595597c03b37c3
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-12-07 00:16:46 -0600

    Ejemplos funciones puras

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programación Funcional con JS
+
+En este repositorio exploraremos el estilo de programación funcional con JS.
---
+++ funciones-puras/basicos.js
@@ -0,0 +1,9 @@
+// Ejemplos de funciones puras
+
+const interesCompuesto = (monto, tasa, periodos) => {
+    monto * Math.pow(tasa, periodos)
+}
+
+const duplicar = (x) => x * 2
+
+const obtenerSaludo = 
